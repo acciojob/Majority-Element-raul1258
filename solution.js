18dea60fdb90f37c367dd32719df964ace47e6c3
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2022-07-11 21:13:14 +0530

    completed

--- solution.js
+++ solution.js
@@ -1,6 +1,21 @@
 // arr = array of numbers
 function major(arr)
 {
-  //your code here
+  let count = 1;
+  let prev = arr[0];
+  for(let i = 1; i < arr.length; i++){
+      if(count === 0){
+          count = 1;
+          prev = arr[i];
+          continue;
+      }
+      if(prev === arr[i]){
+          count++;
+      }else{
+          count--;
+      }
+
+  }
+  return prev;
 }
 module.exports=major;
